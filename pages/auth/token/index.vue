c538d3cd7eb7a81b3b6a80e0b3ecff7d756b1a9c
chobolevel <rodaka123@naver.com>
2023-03-29 23:53:15 +0900

    2023-03-29

---
+++ pages/auth/token/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    TOKEN
+  </div>
+</template>
+
+<script>
+import jwt_decode from "jwt-decode";
+
+export default {
+  created() {
+    const first = this.$route.hash.split('&')[0].split('=')[1]
+
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click.prevent="handleClick">MS LOGIN</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      // window.location.replace(`${process.env.MS_AUTH_URL}?client_id=${process.env.CLIENT_ID}&response_type=token&scope=user.read`)
+      console.log(this.$route.fullPath)
+    }
+  }
+}
+</script>
